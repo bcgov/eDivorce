080b4be65bc0228766fb808fe2b46202eacd4c47
inna.zhurba <inna@oxd.com>
2022-11-21 11:31:26 -0800

    DIV-1440: Downgraded graphql libs

--- edivorce/apps/core/tests/test_api.py
+++ edivorce/apps/core/tests/test_api.py
@@ -338,15 +338,15 @@ class GraphQLAPITest(GraphQLTestCase):
         self._login()
         response = self.query('{documents{id file}}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertContainsError("Cannot query field 'id'", response)
-        self.assertContainsError("Cannot query field 'file'", response)
+        self.assertContainsError('Cannot query field "id"', response)
+        self.assertContainsError('Cannot query field "file"', response)
 
     def test_must_specify_doctype_partycode(self):
         self._login()
         response = self.query('{documents{filename}}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertContainsError("argument 'docType' of type 'String!' is required, but it was not provided", response)
-        self.assertContainsError("argument 'partyCode' of type 'Int!' is required, but it was not provided", response)
+        self.assertContainsError('Field "documents" argument "docType" of type "String!" is required but not provided', response)
+        self.assertContainsError('Field "documents" argument "partyCode" of type "Int!" is required but not provided', response)
 
     def test_get_only_returns_user_form_docs(self):
         self._login()
@@ -480,10 +480,8 @@ class GraphQLAPITest(GraphQLTestCase):
         response = self.query(query, input_data=input_data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Commenting out the tests that are supposed to succeed but
-        # GraphQL errors are not propagated
-        # self.assertResponseHasErrors(response)
-        # self.assertContainsError('there must be the same number of files', response)
+        self.assertResponseHasErrors(response)
+        self.assertContainsError('there must be the same number of files', response)
 
     def assertContainsError(self, msg, response):
         content = json.loads(response.content)
