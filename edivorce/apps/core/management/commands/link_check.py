b1d8d93e6606b05b4de3c352d4aab1741efe65e7
Dylan Rogowsky <dylan@oxd.com>
2024-08-21 10:27:39 -0600

    DIV-16: Refactor link checker

--- edivorce/apps/core/management/commands/link_check.py
+++ edivorce/apps/core/management/commands/link_check.py
@@ -1,9 +1,8 @@
 import os
 import sys
-from urllib.request import urlopen
 
+import requests
 from bs4 import BeautifulSoup
-
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -11,47 +10,56 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Checks links in the eDivorce application.'
 
-    def _check_link(self, address):
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+    }
+
+    def _check_link(self, session, address):
         try:
-            resp = urlopen(address) # nosec - This is for internal use only to check for broken links.
-            if resp.status in [400, 404, 403, 408, 409, 501, 502, 503]:
-                return f"{resp.status} - {resp.reason}"
-        except Exception as e:
-            return f"{e}"
+            print(f'Checking link: {address}')
+            response = session.get(address, headers=self.HEADERS, timeout=15)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return str(e)
         return None
 
     def handle(self, *args, **options):
+        session = requests.Session()
+        links_to_check = {}
         errors = []
 
-        for root, directory, files in os.walk(settings.BASE_DIR + '/apps/core/templates/'):
+        for root, _, files in os.walk(os.path.join(settings.BASE_DIR, 'apps/core/templates/')):
             for file in files:
-                if '.html' in file:
+                if file.endswith('.html'):
                     file_path = os.path.join(root, file)
+                    print(f'Parsing: {file_path}')
 
-                    fs = open(file_path)
+                    with open(file_path, 'r') as fs:
+                        soup = BeautifulSoup(fs, 'html.parser')
+                        for link in soup.find_all('a', href=True):
+                            href = link['href']
+                            if href.startswith('http') and 'localhost' not in href:
+                                links_to_check.setdefault(href, {'href': href, 'filename': []})['filename'].append(file_path)
 
-                    print('Parsing: ' + fs.name)
+        for link_info in links_to_check.values():
+            status = self._check_link(session, link_info['href'])
+            if status:
+                errors.append({'link': link_info['href'], 'error': status, 'file': link_info['filename']})
 
-                    soup = BeautifulSoup(fs, 'html.parser')
-                    links = soup.find_all('a', href=True)
-                    for link in links:
-                        if link is None:
-                            continue
-                        if link['href'].startswith('http'):
-                            filename = str(fs.name)
-
-                            status = self._check_link(link['href'])
-                            if status:
-                                errors.append({
-                                    'link': link['href'],
-                                    'error': status,
-                                    'file': filename
-                                })
-
-        if len(errors) > 0:
+        if errors:
+            print('\nErrors found:\n')
             for error in errors:
                 print('-------------------------------------------------------------')
-                print(f'File: {error["file"]}')
-                print(f'link: {error["link"]}')
-                print(f'Error: {error["error"]}\r\n')
+                print(f"File: {', '.join(error['file'])}")
+                print(f"Link: {error['link']}")
+                print(f"Error: {error['error']}")
             sys.exit(1)
